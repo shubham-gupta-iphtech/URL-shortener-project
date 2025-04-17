cd2e89a4e5beb7fc1148fed137dbe102ce2b3c99
Shubham Gupta <shubham.gupta@iphtechnologies.com>
2025-04-17 14:45:05 +0530

    place your changes here

--- router/urlRoutes.js
+++ router/urlRoutes.js
@@ -3,51 +3,49 @@ import Url from "../model/url.js";
 import { nanoid } from "nanoid";
 const router = express.Router();
 
-router.post('/shorten', async (req, res)=> 
-{
-    const { urlReceived} = req.body;
+router.post('/shorten', async (req, res) => {
+    const { urlReceived } = req.body;
     const longUrl = urlReceived.trim();
     const existing = await Url.findOne({ longUrl });
-    if(existing) return res.json({message: "This url is already shortened."});
+    if (existing) return res.json({ message: "This url is already shortened." });
 
     const BASE_URL = process.env.BASE_URL;
     const urlCode = nanoid(7);
     const shortUrl = `${BASE_URL}/${urlCode}`;
 
-    const url = new Url({longUrl, shortUrl, urlCode});
+    const url = new Url({ longUrl, shortUrl, urlCode });
     await url.save();
 
-    res.json({message: `${shortUrl}`});
+    res.json({ message: `${shortUrl}` });
 })
 
-router.post('/shortenbyalias', async (req, res)=> 
-    {
-        const {urlReceived} = req.body;
-        const {alias} = req.body;
-        const urlCode = alias.trim();
-        const longUrl = urlReceived.trim();
-        const existingcode = await Url.findOne({ urlCode });
-        if(existingcode) return res.json({message: "this alias has been aleady used."});
+router.post('/shortenbyalias', async (req, res) => {
+    const { urlReceived } = req.body;
+    const { alias } = req.body;
+    const urlCode = alias.trim();
+    const longUrl = urlReceived.trim();
+    const existingcode = await Url.findOne({ urlCode });
+    if (existingcode) return res.json({ message: "this alias has been aleady used." });
 
-        const existing = await Url.findOne({ longUrl });
-        if(existing) return res.json({message: "This url is already shortened."});
-    
-        const BASE_URL = process.env.BASE_URL;
-        
-        const shortUrl = `${BASE_URL}/${urlCode}`;
-    
-        const url = new Url({longUrl, shortUrl, urlCode});
-        await url.save();
-    
-        res.json({message: `${shortUrl}`});
-    })
+    const existing = await Url.findOne({ longUrl });
+    if (existing) return res.json({ message: "This url is already shortened." });
 
-router.get('/:code', async (req,res) => {
-    
+    const BASE_URL = process.env.BASE_URL;
+
+    const shortUrl = `${BASE_URL}/${urlCode}`;
+
+    const url = new Url({ longUrl, shortUrl, urlCode });
+    await url.save();
+
+    res.json({ message: `${shortUrl}` });
+})
+
+router.get('/:code', async (req, res) => {
+
     const urlCode = req.params.code;
-    const url = await Url.findOne({urlCode});
-    if(url) return res.redirect(url.longUrl);
-    res.status(404).json({message: "url not found"});
+    const url = await Url.findOne({ urlCode });
+    if (url) return res.redirect(url.longUrl);
+    res.status(404).json({ message: "url not found" });
 });
 
 export default router;
